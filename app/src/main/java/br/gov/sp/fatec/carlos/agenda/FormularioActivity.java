bc6f3d63acc3ce11d9ac4b8c9f27f9f972bb372a
Carlos Eduardo Carvalho <carloseduardoweb@yahoo.com.br>
2018-09-28 03:44:23 -0300

    Initial commit

---
+++ app/src/main/java/br/gov/sp/fatec/carlos/agenda/FormularioActivity.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.carlos.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
---
+++ app/src/main/java/br/gov/sp/fatec/carlos/agenda/ListaAlunosActivity.java
@@ -0,0 +1,20 @@
+package br.gov.sp.fatec.carlos.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Carlos Eduardo", "Clévia Layana", "Viviane Aparecida", "Carlos Alberto"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+}
